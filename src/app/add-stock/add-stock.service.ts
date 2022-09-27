f8b2817a3c413e747665d08ab565ce7c67c82ef5
Jeff Justice <jeff@justicewebtech.com>
2022-09-27 14:48:26 -0700

    comment added

--- src/app/add-stock/add-stock.service.ts
+++ src/app/add-stock/add-stock.service.ts
@@ -14,9 +14,14 @@ export class AddStockService {
     ) { }
 
   addStock(stockInput: string): void{
+    // update stocks array
     let stocks:string[] = this.localStorageService.getStocks();
     stocks.push(stockInput);
+
+    // update local storage
     this.localStorageService.saveStocks(stocks);
+
+    // update behaviorsubject with response for new stock which updates view for single item added
     this.stocksService.getQuote(stockInput).subscribe(result => {
       this.stocksService.quotes.push(result);
       this.stocksService.allQuotes$.next(this.stocksService.quotes);
