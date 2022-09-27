3329a2be89babd23c5dc68ed508a9ae639729a54
Jeff Justice <jeff@justicewebtech.com>
2022-09-27 15:08:04 -0700

    delete functionality

--- src/app/stock-info/stock-info.component.ts
+++ src/app/stock-info/stock-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { StockQuote } from '../models';
+import { LocalStorageService } from '../local-storage.service';
+import { QuoteInfo, StockQuote } from '../models';
 import { StocksService } from '../stocks.service';
 
 @Component({
@@ -12,10 +13,29 @@ export class StockInfoComponent implements OnInit {
 
   allQuotes$ = new BehaviorSubject<StockQuote[]>([]);
 
-  constructor(private stocksService:StocksService) { }
+  constructor(
+    private stocksService:StocksService,
+    private localStorageService: LocalStorageService
+    ) { }
 
   ngOnInit(): void {
     this.allQuotes$ = this.stocksService.allQuotes$;
+  }
+
+  deleteItem(symbol:string): void{
+    let quotes: StockQuote[] = JSON.parse(JSON.stringify(this.stocksService.quotes));
+    const quoteIdx = quotes.findIndex(quote => quote.company.symbol.toLocaleLowerCase() === symbol.toLocaleLowerCase());
+    quotes.splice(quoteIdx,1);
+
+    // update local storage - array of symbols (string[])
+    this.localStorageService.saveStocks(quotes.map(quote => quote.company.symbol));
+
+    // update behavior subject - StockQuote[]
+    this.stocksService.allQuotes$.next(quotes);
+  }
+
+  sentiment(symbol: string): void {
+
   }
 
 }
