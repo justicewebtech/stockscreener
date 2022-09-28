60f604c8ec5e5fba8316600cc9903b0e8649a61f
Jeff Justice <jeff@justicewebtech.com>
2022-09-27 17:14:55 -0700

    sentiment work

--- src/app/stock-sentiment/stock-sentiment.component.ts
+++ src/app/stock-sentiment/stock-sentiment.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { combineLatest, mergeMap, Observable, of } from 'rxjs';
+import { StockQuote } from '../models';
+import { StocksService } from '../stocks.service';
 
 @Component({
   selector: 'app-stock-sentiment',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockSentimentComponent implements OnInit {
 
-  constructor() { }
+  sentiment$: Observable<any> = of();
+  defaultQuote: StockQuote = {
+    company:{
+      description: "",
+      displaySymbol: "",
+      symbol: "",
+      type: ""
+    },
+    quote:{
+      "c": 0,
+      "d": 0,
+      "dp": 0,
+      "h": 0,
+      "l": 0,
+      "o": 0,
+      "pc": 0,
+      "t": 0,
+    }
+  };
+  quote: StockQuote = Object.assign(this.defaultQuote);
+
+  constructor(
+    private stocksService: StocksService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    //TODO: Finish implementing this logic to display information in sentiment component
+    this.route.paramMap
+      .pipe(
+        mergeMap((params: ParamMap) => {
+          let symbol = params.get('symbol') ?? "";
+          let sentiment$ = this.stocksService.getSentiment(symbol);
+          let companyInfo$ = this.stocksService.getCompanyInfo(symbol);
+          return combineLatest([companyInfo$, sentiment$]);
+        })
+      ).subscribe(x => console.log(x))
+
   }
 
 }
--- src/app/stocks.service.ts
+++ src/app/stocks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient} from '@angular/common/http';
-import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable, of } from 'rxjs';
 import { LocalStorageService } from './local-storage.service';
 import { CompanyInfo, QuoteInfo, StockQuote } from './models';
 
@@ -33,11 +33,7 @@ export class StocksService {
     const nameUrl = `https://finnhub.io/api/v1/search?q=${stock}&token=${this.apiKey}`;
 
     let quoteData$ = this.http.get<QuoteInfo>(quoteUrl,httpOptions);
-    let companyName$ = this.http.get<{'count': number, 'result': CompanyInfo[]}>(nameUrl,httpOptions)
-      .pipe(
-        map(x => x.result[0])
-        //.find(({displaySymbol}) => displaySymbol === stock)) - returns CompanyInfo | undefined - so we assume we can use first array element in return instead
-      );
+    let companyName$ = this.getCompanyInfo(stock);
 
     return combineLatest([companyName$, quoteData$])
       .pipe(
@@ -51,10 +47,41 @@ export class StocksService {
       );
   }
 
+  getCompanyInfo(stock: string): Observable<CompanyInfo>{
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json; charset=UTF-8');
+    headers.append('Access-Control-Allow-Origin', '*');
+    let httpOptions = {headers: headers};
+    const nameUrl = `https://finnhub.io/api/v1/search?q=${stock}&token=${this.apiKey}`;
+
+    return this.http.get<{'count': number, 'result': CompanyInfo[]}>(nameUrl,httpOptions)
+      .pipe(
+        map(x => x.result[0])
+        //.find(({displaySymbol}) => displaySymbol === stock)) - returns CompanyInfo | undefined - so we assume we can use first array element in return instead
+      );
+
+  }
+
   getAllQuotes(): Observable<StockQuote[]> {
     const stocks: string[] = this.localStorageService.getStocks();
     let requests: Observable<StockQuote>[] = [];
     stocks.forEach(stock => requests.push(this.getQuote(stock)));
     return combineLatest(requests);
+  }
+
+  getSentiment(symbol: string): Observable<any>{
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json; charset=UTF-8');
+    headers.append('Access-Control-Allow-Origin', '*');
+    let httpOptions = {headers: headers};
+    let current = new Date();
+    let previous = new Date();
+    previous.setMonth(previous.getMonth() - 2);
+
+    let currentDateString = current.toISOString().split('T')[0];
+    let previousDateString = previous.toISOString().split('T')[0];
+
+    const url = `https://finnhub.io/api/v1/stock/insider-sentiment?symbol=${symbol}&from=${previousDateString}&to=${currentDateString}&token=${this.apiKey}`;
+    return this.http.get<any>(url,httpOptions);
   }
 }
